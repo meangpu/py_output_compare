7542ce1d36f7b099d507e6745ef16027879c2589
meangpu <51825872+meangpu@users.noreply.github.com>
2024-07-01 06:08:24 +0700

    fix time out setting

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,5 @@
 python .\setup.py sdist bdist_wheel
-pip install .\dist\py_output_compare-0.6-py3-none-any.whl --force-reinstall
+pip install .\dist\py_output_compare-0.7-py3-none-any.whl --force-reinstall
 
 
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="py_output_compare",
-    version=0.6,
+    version=0.7,
     description="a script that help compare output of 2 python script, I apply this to help grading student code compare to teacher",
     long_description=description_readme,
     long_description_content_type="text/markdown",
