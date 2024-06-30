dd62c373766b2a56037442e45f1171994446d599
meangpu <51825872+meangpu@users.noreply.github.com>
2024-07-01 04:16:33 +0700

    stable

--- py_output_compare/compare_file.py
+++ py_output_compare/compare_file.py
@@ -3,6 +3,7 @@ from py_output_compare.user_input_case import InputCase
 from py_output_compare.highlight import highlight_diff
 from py_output_compare.normalize_file_output import normalize_output
 from py_output_compare.get_score import get_score_emoji
+from find_file import find_first_file
 
 
 def get_run_output(filename, input_data, timeout_setting=6):
@@ -68,3 +69,32 @@ def get_compare_output(
 
     final_compare_result = "\n".join(result)
     return final_compare_result
+
+
+def main():
+    student_file = find_first_file("bad.py")
+    student_file_good = find_first_file("good.py")
+    teacher_file = find_first_file("teacher_file.py")
+
+    case1_input = [8.2, 1.8]
+    case_input_int = [8.2, 999, 9334]
+
+    test_cases = [
+        InputCase(case1_input),
+        InputCase(case_input_int),
+    ]
+
+    print(teacher_file)
+
+    fail_student = get_compare_output(student_file, teacher_file, test_cases)
+    pass_student = get_compare_output(student_file_good, teacher_file, test_cases)
+    no_test_input = get_compare_output(student_file_good, teacher_file)
+
+    print(fail_student)
+    print(pass_student)
+    print(no_test_input)
+
+
+if __name__ == "__main__":
+
+    main()
