7c415845b5cdaca20ce1aa21bc0fe4743488c406
meangpu <51825872+meangpu@users.noreply.github.com>
2024-07-01 01:28:06 +0700

    Update __init__.py

--- py_output_compare/__init__.py
+++ py_output_compare/__init__.py
@@ -1,9 +1,9 @@
+from test_class import Test
+from normalize_output import normalize_output, normalize_output_no_lower
+from highlight_diff import highlight_diff
+from find_file import find_files, find_first_file_contain_id, find_first_file
+
 from compare_file import (
     get_compare_output,
     get_run_output,
 )
-
-from normalize_output import normalize_output, normalize_output_no_lower
-from highlight_diff import highlight_diff
-from test_class import Test
-from find_file import find_files, find_first_file_contain_id, find_first_file
