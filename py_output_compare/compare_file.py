374fddc0d14948051b340b90fe82d0c928db4123
meangpu <51825872+meangpu@users.noreply.github.com>
2024-07-01 05:56:25 +0700

    update

--- py_output_compare/compare_file.py
+++ py_output_compare/compare_file.py
@@ -1,53 +1,9 @@
-import subprocess
 from py_output_compare.user_input_case import InputCase
 from py_output_compare.highlight import highlight_diff
 from py_output_compare.normalize_file_output import normalize_output
 from py_output_compare.get_score import get_score_emoji
 from py_output_compare.find_file import find_first_file
-
-
-def get_run_output_by_search_file_name(
-    filename, input_data=[InputCase("")], timeout_setting=6
-):
-    """_summary_
-
-    Args:
-        filename (_type_): get file name instead of path, it will try to find by itself
-        input_data (_type_):
-        timeout_setting (int, optional):. Defaults to 6.
-    """
-    path = find_first_file(filename)
-    return get_run_output_by_path(path, input_data, timeout_setting)
-
-
-def get_run_output_by_path(file_path, input_data, timeout_setting=6):
-    output_lines = ""
-    try:
-        refactor_input = "\n".join((map(str, input_data)))
-        result = subprocess.run(
-            ["python", file_path],
-            input=refactor_input,
-            capture_output=True,
-            text=True,
-            timeout=timeout_setting,
-            encoding="utf-8",
-        )
-        output_lines = result.stdout
-        output_lines += result.stderr
-
-        if len(output_lines) == 0:
-            raise Exception("your file give no output")
-
-        return output_lines
-    except EOFError:
-        output_lines += "🔚 End of file Error"
-        return output_lines
-    except subprocess.TimeoutExpired:
-        output_lines += "💀 Timed out!!!"
-        return output_lines
-    except Exception as e:
-        output_lines += f"😲 Encountered an exception: {str(e)}"
-        return output_lines
+from py_output_compare.get_file_run_output import get_run_output_by_path
 
 
 def get_compare_output_by__search_file_name(
@@ -55,11 +11,12 @@ def get_compare_output_by__search_file_name(
     filename_2,
     user_input_list=[InputCase("")],
     do_normalize_output=False,
+    timeout=6,
 ):
     filepath_1 = find_first_file(filename_1)
     filepath_2 = find_first_file(filename_2)
     return get_compare_output_by_path(
-        filepath_1, filepath_2, user_input_list, do_normalize_output
+        filepath_1, filepath_2, user_input_list, do_normalize_output, timeout
     )
 
 
@@ -68,13 +25,18 @@ def get_compare_output_by_path(
     file_path_2,
     user_input_list=[InputCase("")],
     do_normalize_output=False,
+    timeout=6,
 ):
     result = []
     score = 0
     result.append("=" * 80)
     for user_input in user_input_list:
-        file_output_1 = get_run_output_by_path(file_path_1, user_input.case_input)
-        file_output_2 = get_run_output_by_path(file_path_2, user_input.case_input)
+        file_output_1 = get_run_output_by_path(
+            file_path_1, user_input.case_input, timeout
+        )
+        file_output_2 = get_run_output_by_path(
+            file_path_2, user_input.case_input, timeout
+        )
 
         if do_normalize_output:
             file_output_2 = normalize_output(file_output_2)
--- setup.ps1
+++ setup.ps1
@@ -1,2 +1,9 @@
 python .\setup.py sdist bdist_wheel
 pip install .\dist\py_output_compare-0.6-py3-none-any.whl --force-reinstall
+
+
+
+# twine check dist/*
+# twine upload dist/*
+# To enter api token cannot use ctrl+v or right click, need to use Edit>paste
+
