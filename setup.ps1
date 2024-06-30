03e5839b31f7280bb10c608d1c861738bedb94dc
meangpu <51825872+meangpu@users.noreply.github.com>
2024-07-01 03:14:42 +0700

    update command

--- setup.ps1
+++ setup.ps1
@@ -1,2 +1,2 @@
 python .\setup.py sdist bdist_wheel
-pip install .\dist\py_output_compare-0.2-py3-none-any.whl --force-reinstall
+pip install .\dist\py_output_compare-0.4-py3-none-any.whl --force-reinstall
--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    description_readme = f.read()
+
 setup(
     name="py_output_compare",
     description="a script that help compare output of 2 python script, I apply this to help grading student code compare to teacher",
-    version=0.2,
+    long_description=description_readme,
+    long_description_content_type="text/markdown",
+    version=0.4,
     package_dir={"": "py_output_compare"},
     packages=find_packages(where="py_output_compare"),
     author="meangpu",
     license="MIT",
     url="https://github.com/meangpu/py_output_compare",
+    entry_points={
+        "console_scripts": [
+            "py_output_compare-hello = py_output_compare.get_score:get_score_emoji"
+        ]
+    },
 )
