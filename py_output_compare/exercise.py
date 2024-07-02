5e333c5beacdc7aee3061c1f8b5a7efa3cf515b4
meangpu <51825872+meangpu@users.noreply.github.com>
2024-07-03 02:52:42 +0700

    stable

--- py_output_compare/exercise.py
+++ py_output_compare/exercise.py
@@ -10,20 +10,20 @@ class Exercise:
 
     def get_score_all_exercise(self) -> str:
         final_result = []
-        for exercise in self.exercises:
-            final_result.append(exercise.lab_name)
-            final_result.append(exercise.get_score_all())
+        for problem in self.problems:
+            final_result.append(problem.problem_name)
+            final_result.append(problem.get_score_all())
             final_result.append("=" * 80)
         return "\n".join(final_result)
 
     def get_score_id(self, student_id: str) -> str:
         final_result = []
-        for exercise in self.exercises:
-            final_result.append(exercise.get_score_id(student_id))
+        for problem in self.problems:
+            final_result.append(problem.get_score_id(student_id))
         return "\n".join(final_result)
 
     def get_output_id(self, student_id: str) -> str:
         final_result = []
-        for exercise in self.exercises:
-            final_result.append(exercise.get_output_id(student_id))
+        for problem in self.problems:
+            final_result.append(problem.get_output_id(student_id))
         return "\n".join(final_result)
--- py_output_compare/problem.py
+++ py_output_compare/problem.py
@@ -6,21 +6,23 @@ from py_output_compare.test_case import TestCase
 class Problem:
     def __init__(
         self,
-        lab_name,
+        problem_name,
         input_cases=[TestCase("")],
         do_normalize_input=False,
         timeout_setting=6,
         teacher_name="manee-2024",
     ):
-        self.lab_name = lab_name
+        self.problem_name = problem_name
         self.input_cases = input_cases
         self.do_normalize_input = do_normalize_input
         self.teacher_name = teacher_name
         self.timeout_setting = timeout_setting
 
     def get_score_all(self) -> str:
-        teacher_file_path = find_first_file_contain_id(self.lab_name, self.teacher_name)
-        all_student = find_files(self.lab_name)
+        teacher_file_path = find_first_file_contain_id(
+            self.problem_name, self.teacher_name
+        )
+        all_student = find_files(self.problem_name)
         result = []
 
         for student_file_path in all_student:
@@ -37,8 +39,10 @@ class Problem:
         return "\n".join(result)
 
     def get_output_id(self, student_id) -> str:
-        student_file_path = find_first_file_contain_id(self.lab_name, student_id)
-        teacher_file_path = find_first_file_contain_id(self.lab_name, self.teacher_name)
+        student_file_path = find_first_file_contain_id(self.problem_name, student_id)
+        teacher_file_path = find_first_file_contain_id(
+            self.problem_name, self.teacher_name
+        )
         result = get_compare_output_by_path(
             student_file_path,
             teacher_file_path,
@@ -49,8 +53,10 @@ class Problem:
         return result
 
     def get_score_id(self, student_id) -> str:
-        teacher_file_path = find_first_file_contain_id(self.lab_name, self.teacher_name)
-        student_file_path = find_first_file_contain_id(self.lab_name, student_id)
+        teacher_file_path = find_first_file_contain_id(
+            self.problem_name, self.teacher_name
+        )
+        student_file_path = find_first_file_contain_id(self.problem_name, student_id)
 
         score_num, score_emoji = get_score_by_path(
             student_file_path,
