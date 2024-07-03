8a614f1770b369c0df2653b29f39e71bcd7fff9f
meangpu <51825872+meangpu@users.noreply.github.com>
2024-07-03 15:29:10 +0700

    add count file

--- py_output_compare/find_file.py
+++ py_output_compare/find_file.py
@@ -2,7 +2,11 @@ import os
 
 
 def find_files(
-    filename, base_dir="./", case_sensitive=True, find_all=True, skip_text=None
+    filename: str,
+    base_dir: str = "./",
+    case_sensitive: bool = True,
+    find_all: bool = True,
+    skip_text: str = None,
 ):
     filename_lower = filename.lower() if not case_sensitive else None
     skip_text_lower = (
@@ -31,6 +35,43 @@ def find_files(
                 yield full_path
                 if not find_all:
                     return
+
+
+def count_files(
+    filename: str,
+    base_dir: str = "./",
+    case_sensitive: bool = True,
+    skip_text: str = None,
+) -> int:
+
+    count = 0
+
+    filename_lower = filename.lower() if not case_sensitive else None
+    skip_text_lower = (
+        skip_text.lower() if skip_text and not case_sensitive else skip_text
+    )
+
+    for root, _, files in os.walk(base_dir):
+        root_to_check = root if case_sensitive else root.lower()
+        if skip_text and (
+            (case_sensitive and skip_text in root)
+            or (not case_sensitive and skip_text_lower in root_to_check)
+        ):
+            continue
+
+        for file in files:
+            file_to_check = file if case_sensitive else file.lower()
+            if (case_sensitive and file == filename) or (
+                not case_sensitive and file_to_check == filename_lower
+            ):
+                if skip_text and (
+                    (case_sensitive and skip_text in file)
+                    or (not case_sensitive and skip_text_lower in file_to_check)
+                ):
+                    continue
+                count += 1
+
+    return count
 
 
 def find_first_file(filename, base_dir="./", case_sensitive=True):
